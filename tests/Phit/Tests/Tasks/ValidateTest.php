f732aa0359a960fcee4f8bdfa5cea1a67001ed81
Guillaume Maïssa <guillaume.maissa@phabriks.com>
2012-07-12 11:13:10 +0200

    add / update file / class / method headers

--- tests/Phit/Tests/Tasks/ValidateTest.php
+++ tests/Phit/Tests/Tasks/ValidateTest.php
@@ -1,12 +1,38 @@
 <?php
+/**
+ * Phit Tests classes
+ *
+ * PHP VERSION 5
+ *
+ * @category  Phit
+ * @package   Phit.Tests
+ * @author    Guillaume Maïssa <guillaume.maissa@phabriks.com>
+ * @copyright 2012 Phabriks
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version   SVN: $Id:$
+ * @link      http://phit.phabriks.fr
+ */
 
 namespace Phit\Tests\Tasks;
 
 use Phit\Phit;
 use Phit\Tester\TaskTester;
 
+/**
+ * Validate Task Test class
+ *
+ * @category  Phit
+ * @package   Phit.Tests
+ * @author    Guillaume Maïssa <guillaume.maissa@phabriks.com>
+ * @copyright 2012 Phabriks
+ */
 class ValidateTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Execute test method data provider
+     *
+     * @return array
+     */
     public function executeDataProvider()
     {
         return array(
@@ -16,11 +42,18 @@ class ValidateTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * Test Validate task Execute method
+     *
+     * @param mixed  $path            the path the the project root directory
+     * @param string $expectedTextMsg part of the message that should be displayed
+     *                                while testing project conf file validity
+     * @return void
      * @dataProvider executeDataProvider
      */
     public function testExecute($path, $expectedTextMsg)
     {
-        $phit = Phit::getInstance($path);
+        $phit = Phit::getInstance();
+        $phit->setProjectRootDir($path);
         $task = $phit->getApplication()->get('validate');
 
         $taskTester = new TaskTester($task);
@@ -30,13 +63,5 @@ class ValidateTest extends \PHPUnit_Framework_TestCase
             $taskTester->getDisplay(),
             '->execute() validate the Phit project configuration file'
         );
-
-        $taskTester->execute(array('command' => $task->getName()));
-        $this->assertRegExp(
-            '/Start validating phit conf file/',
-            $taskTester->getDisplay(),
-            '->execute() validate the Phit project configuration file'
-        );
-        unset($phit);
     }
 }
